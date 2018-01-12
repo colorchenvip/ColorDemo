7687e05e2af0de56a128cccbb3048735add7f06f
陈庆 <wxchenq@yutong.com>
2018-01-12 09:13:29 +0800

    初始化导入

---
+++ app/src/main/java/com/colorchen/demo/MainActivity.java
@@ -0,0 +1,18 @@
+package com.colorchen.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * name：MainActivity
+ * @author: ChenQ
+ * @date: 2018-1-12
+ * @email: wxchenq@yutong.com
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
