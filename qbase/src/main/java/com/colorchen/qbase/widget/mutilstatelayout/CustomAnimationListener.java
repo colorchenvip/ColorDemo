5acb9f029d16a593e40da4b823a185cecf10a042
陈庆 <wxchenq@yutong.com>
2018-03-31 18:41:32 +0800

    base功能不提交

---
+++ qbase/src/main/java/com/colorchen/qbase/widget/mutilstatelayout/CustomAnimationListener.java
@@ -0,0 +1,21 @@
+package com.colorchen.qbase.widget.mutilstatelayout;
+
+import android.view.animation.Animation;
+
+
+@SuppressWarnings("PMD.UncommentedEmptyMethodBody")
+public class CustomAnimationListener implements Animation.AnimationListener {
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+    }
+
+}
